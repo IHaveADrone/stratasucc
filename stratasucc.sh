54433e13210b174418c07881317d1a7e3805a587
IHaveADrone <IHaveADrone@users.noreply.github.com>
2019-09-11 10:19:11 -0400

    Create stratasucc.sh

---
+++ stratasucc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+usage()
+{
+  echo "Read and write to Stratasucc(TM) EEPROM chips"
+  echo "Usage: strat.sh [-rw]"
+  echo "-r              Read from EEPROM"
+  echo "-w              Write to EEPROM"
+  echo "-h              Display this help text and exit"
+  exit
+}

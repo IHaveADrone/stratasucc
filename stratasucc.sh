559815809c38286cff6528709c4e8891fb05d253
IHaveADrone <IHaveADrone@users.noreply.github.com>
2019-09-11 10:43:50 -0400

    Update stratasucc.sh

--- stratasucc.sh
+++ stratasucc.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 
-usage()
-{
+usage() {
   echo "Read and write to Stratasucc(TM) EEPROM chips"
-  echo "Usage: strat.sh [-rw]"
+  echo "Usage: stratasucc.sh [-rw]"
   echo "-r              Read from EEPROM"
   echo "-w              Write to EEPROM"
   echo "-h              Display this help text and exit"
   exit
 }
+
+function GetChipID() {
+  #cd in to that pesky serial number folder
+  cd /sys/bus/w1/devices/w1_bus_master1/ && cd $(ls | grep 23-)
+  #Get chip ID (non dashed one)
+  ID=$(xxd -p id)
+  echo 'found chip: '$ID
+}
+
+function ReadEE() {
+  GetChipID()
+
+  cp eeprom ~/strat-chip/read.bin
+  cd ~/strat-chip
+  stratatools eeprom_decode -t prodigy -e $ID read.bin read.txt
+
+  echo 'done!'
+}
+
+function WriteEE() {
+  GetChipID()
+
+  cd ~/strat-chip
+
+  stratatools eeprom_encode -t prodigy -e $ID write.txt write.bin
+
+  cd /sys/bus/w1/devices/w1_bus_master1/
+  cd $ID
+
+  sudo cp ~/strat-chip/write.bin eeprom
+  echo 'done!'
+}
+
+
+while getopts ':rw?h' c
+do
+  case $c in
+    r) ReadEE
+      exit ;;
+    w) WriteEE
+      exit ;;
+    h|?) usage ;;
+    :) usage ;;
+  esac
+done
